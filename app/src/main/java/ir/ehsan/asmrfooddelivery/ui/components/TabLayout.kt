daea8617f5e89148b633c126ebccb61e719dccd0
ehsan narmani <ehsan.enk.narmani@gmail.com>
2023-09-11 17:11:50 +0330

    part 2

--- app/src/main/java/ir/ehsan/asmrfooddelivery/ui/components/TabLayout.kt
+++ app/src/main/java/ir/ehsan/asmrfooddelivery/ui/components/TabLayout.kt
@@ -10,10 +10,12 @@ import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -22,13 +24,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun TabLayout(
     selectedIndex: Int = 0,
     items: List<Pair<String, @Composable () -> Unit>>,
-    onTabClick: (Int) -> Unit
+    onTabClick: (Int) -> Unit,
+    textHeight:Dp = 35.dp,
+    indicatorPadding:PaddingValues = PaddingValues()
 ) {
     Column {
         Row(
@@ -38,7 +43,7 @@ fun TabLayout(
         ) {
             items.forEachIndexed { index, pair ->
                 Column(modifier = Modifier
-                    .height(35.dp)
+                    .height(textHeight)
                     .fillMaxWidth()
                     .weight(1f)
                     .clickable(
@@ -66,6 +71,7 @@ fun TabLayout(
                                 .fillMaxWidth()
                                 .clip(CircleShape)
                                 .height(3.dp)
+                                .padding(indicatorPadding)
                                 .background(MaterialTheme.colorScheme.primary)
                         )
                     }
